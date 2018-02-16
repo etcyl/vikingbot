d7ebe7460908c515ce56b9f67eebbae122651e6c
Matt Fleetwood <32446381+etcyl@users.noreply.github.com>
2018-02-16 03:45:18 -0800

    Update svc_training.py

--- examples/svc_training.py
+++ examples/svc_training.py
@@ -2,27 +2,23 @@
 """
 Created on Thu Feb 15 19:00:34 2018
 
-Uses a Support Vector Machine (SVM), C-Support Vector Classification or SVC,
-from scikit-learn to identify obstacles in front of the Vikingbot.
+Uses a Support Vector Machine (SVM) from scikit-learn to identify obstacles in front of the Vikingbot.
 
 @author: Etcyl
 """
 
 from sklearn import svm
 import pandas as pd
+import pickle
 
 #Make a SVM classifier 
 SVM_class = svm.SVC()
 
-#Data in scikit-learn is typically a Numpy array, but here it is a Pandas DataFrame
-#Each input variable represents a sonic sensor reading
-#If the sensor reading is between 2 and 30, then it is treated as 1 otherwise it is treated as 0
-#Inputs A, B, and C represent sensor readings to the left, in front, and to the right of the Vikingbot
 inputs_and_targets = pd.DataFrame({
         'A' : [0, 0, 0, 0, 1, 1, 1, 1],
         'B' : [0, 0, 1, 1, 0, 0, 1, 1],
         'C' : [0, 1, 0, 1, 0, 1, 0, 1],
-        'Targets' : [2, 2, 1, 1, 2, 1, 2, 1]
+        'Targets' : [0, 1, 2, 3, 4, 5, 6, 7]
         })
 
 #Train the SVM model on the inputs and targets
@@ -30,3 +26,7 @@ SVM_class.fit(inputs_and_targets[['A', 'B', 'C']], inputs_and_targets ['Targets'
 
 #Get and print the aaverage performance
 print("The mean accuracy is: ", SVM_class.score(inputs_and_targets[['A', 'B', 'C']], inputs_and_targets ['Targets']))
+
+#Pickle the model for later use on the Vikingbot
+filename = 'svm_class.sav'
+pickle.dump(SVM_class, open(filename, 'wb'))
