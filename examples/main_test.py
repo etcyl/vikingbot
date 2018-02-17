003cdbf3b4a1e51a2aff5ee84d26237b00f61b5b
Matt Fleetwood <32446381+etcyl@users.noreply.github.com>
2018-02-16 16:25:01 -0800

    Update main_test.py

--- examples/main_test.py
+++ examples/main_test.py
@@ -2,8 +2,6 @@
 """
 Created on Fri Feb 16 02:54:09 2018
 
-Main testing file for loading scikit-learn classifiers onto the Vikingbot.
-
 @author: Etcyl
 """
 
@@ -12,7 +10,6 @@ import motor_controller as MC
 import ultrasonic as US
 import RPi.GPIO as GPIO
 import pickle
-import pandas as pd
 
 #Set GPIO mode to BCM
 GPIO.setmode(GPIO.BCM)
@@ -24,9 +21,11 @@ HC_sensor = US.Ultrasonic()
 #DO setup here
 #...
 
-#Get the pickled model
+#Get the pickled models
 filename = 'name.sav'
 perc_class_load = pickle.load(open(filename, 'rb'))
+filename = 'name.sav'
+sensor_vals = pickle.load(open(filename, 'rb'))
 
 #Check front, left, and right of the vb for objects using the sk model
 while(1):
@@ -42,11 +41,12 @@ while(1):
     vb_motor.set_SleepTime(1)
     obj_right = HC_sensor.get_distance()
     bool_right = sf.convert_sensor_reading(obj_right)
-    sensor_vals = pd.DataFrame({
-        'A' : [bool_left],
-        'B' : [bool_front],
-        'C' : [bool_right],
-        })
+    current_A = sensor_vals.iloc[0]['A']
+    current_B = sensor_vals.iloc[0]['B']   
+    current_C = sensor_vals.iloc[0]['C']
+    sensor_vals['A'] = sensor_vals['A'].replace([current_A], bool_left)
+    sensor_vals['B'] = sensor_vals['B'].replace([current_B], bool_front)
+    sensor_vals['C'] = sensor_vals['C'].replace([current_C], bool_right)
     class_label = perc_class_load.predict(sensor_vals[['A', 'B', 'C']])
     if class_label == 1: #Class label is 1, so turn right then forwards
         vb_motor.turnRight()
